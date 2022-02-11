c24d2c934753be72363c54711deb081e3885909c
keanuWheeze <48886873+justarandomguyintheinternet@users.noreply.github.com>
2022-02-11 22:25:55 +0100

    v. 1.7
    - Better no hud and motion blur
    - General better transition experience
    - Added "speed up time during transition" option

--- modules/ui/helpUI.lua
+++ modules/ui/helpUI.lua
@@ -13,7 +13,7 @@ function helpUI.draw(gtaTravel)
     ImGui.Text("Mod wont work / nothing happens")
     gtaTravel.CPS.colorEnd(1)
     ImGui.Separator()
-    
+
     ImGui.TextWrapped("- Make sure you dont have any other mods installed that affect fast traveling, like \"Fast travel from anywhere 2 anywhere\"")
     ImGui.TextWrapped("- Check your \"GTA Travel from:\" settings in the misc settings tab to enable fast travel from anywhere and more")
     ImGui.TextWrapped("- If used in a vehicle you will get teleported regularly, thats just how it works")
@@ -24,19 +24,18 @@ function helpUI.draw(gtaTravel)
     ImGui.Text("Loading screen / stuck after / during transition")
     gtaTravel.CPS.colorEnd(1)
     ImGui.Separator()
-    
+
     ImGui.TextWrapped("- First thing to try especially on slow pcs / with game on hdd, is to lower the max speeds on all paths")
     ImGui.TextWrapped("- If you travel up a bit and then get a loading screen, followed by being stuck at your original position lower your \"Player TP Dist.\" on the upward path settings")
     ImGui.TextWrapped("- If during the downward motion you get a loading screen / get stuck at the target destination lower your \"Player TP Dist.\" on the downward path settings")
     ImGui.EndChild()
 
     gtaTravel.CPS.colorBegin("Text", speedUI.colors.frame)
-    ImGui.BeginChild("help3", speedUI.boxSize.x, 110, true)
+    ImGui.BeginChild("help3", speedUI.boxSize.x, 68, true)
     ImGui.Text("Other issues")
     gtaTravel.CPS.colorEnd(1)
     ImGui.Separator()
-    
-    ImGui.TextWrapped("- If you can see the players head after the transition you can toggle him in the misc settings, or just disable the head during transition completely")
+
     ImGui.TextWrapped("- If you want a smoother motion that is less linear lower the speed increment / play aorund with that value")
     ImGui.EndChild()
 
